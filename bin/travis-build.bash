d77e83c9bd842fff04836004b7f93a3ede94030f
Luiz Felipe F M Costa <luiz@thenets.org>
2017-10-26 07:25:21 -0200

    Update travis-build.bash

--- bin/travis-build.bash
+++ bin/travis-build.bash
@@ -5,21 +5,23 @@ echo "This is travis-build.bash..."
 
 echo "Installing the packages that CKAN requires..."
 sudo apt-get update -qq
-sudo apt-get install solr-jetty
-wget http://ftp.us.debian.org/debian/pool/main/libc/libcommons-fileupload-java/libcommons-fileupload-java_1.3.1-1+deb8u1_all.deb
-sudo dpkg -i libcommons-fileupload-java_1.3.1-1+deb8u1_all.deb
+sudo apt-get install solr-jetty libcommons-fileupload-java
 
 echo "Installing CKAN and its Python dependencies..."
 git clone https://github.com/ckan/ckan
 cd ckan
-if [ $CKANVERSION != 'master' ]
+if [ $CKANVERSION == 'master' ]
 then
-    git checkout release-v$CKANVERSION-latest
+    echo "CKAN version: master"
+else
+    CKAN_TAG=$(git tag | grep ^ckan-$CKANVERSION | sort --version-sort | tail -n 1)
+    git checkout $CKAN_TAG
+    echo "CKAN version: ${CKAN_TAG#ckan-}"
 fi
 python setup.py develop
-pip install -r requirements.txt
-pip install -r dev-requirements.txt
-cd ..
+pip install -r requirements.txt --allow-all-external
+pip install -r dev-requirements.txt --allow-all-external
+cd -
 
 echo "Setting up Solr..."
 # solr is multicore for tests on ckan master now, but it's easier to run tests
@@ -31,7 +33,6 @@ sudo cp ckan/ckan/config/solr/schema.xml /etc/solr/conf/schema.xml
 sudo service jetty restart
 
 echo "Creating the PostgreSQL user and database..."
-sudo chmod -R 777 .
 sudo -u postgres psql -c "CREATE USER ckan_default WITH PASSWORD 'pass';"
 sudo -u postgres psql -c "CREATE USER datastore_default WITH PASSWORD 'pass';"
 sudo -u postgres psql -c 'CREATE DATABASE ckan_test WITH OWNER ckan_default;'
