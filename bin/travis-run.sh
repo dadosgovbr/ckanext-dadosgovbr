b62eddd5546c9b9bb4a991266f0f5f6e6f3f24d8
Luiz Felipe F M Costa <luiz@thenets.org>
2017-11-03 23:26:06 -0200

    Update travis-run.sh
    Front-end tests

--- bin/travis-run.sh
+++ bin/travis-run.sh
@@ -1,2 +1,35 @@
 #!/bin/sh -e
+
+# Test: Check build
+# ===========================================================
 nosetests --ckan --nologcapture --with-pylons=subdir/test.ini ckanext/dadosgovbr
+
+# Test: Front-end
+# ===========================================================
+
+# Start CKAN server
+paster serve test-core.ini &
+sleep 5 # Make sure the server has fully started
+
+# Run test
+mocha-phantomjs http://localhost:5000/base/test/index.html
+
+# Did an error occur?
+MOCHA_ERROR=$?
+
+# We are done so kill CKAN
+killall paster
+
+# Test: All nosetests
+# ===========================================================
+nosetests --ckan --reset-db --with-pylons=test-core.ini --nologcapture --with-coverage --cover-package=ckan --cover-package=ckanext ckan ckanext
+
+# Did an error occur?
+NOSE_ERROR=$?
+[ "0" -ne "$MOCHA_ERROR" ] && echo MOCHA tests have failed
+[ "0" -ne "$NOSE_ERROR" ] && echo NOSE tests have failed
+
+
+# Error output to Travis
+# ===========================================================
+exit `expr $MOCHA_ERROR + $NOSE_ERROR`
